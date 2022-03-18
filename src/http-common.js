55b0f7ae29f8640d3d35bf0eee3e496632728927
Cristian Perez <cristian.ap84@gmail.com>
2022-03-17 21:26:07 -0300

    Added env vars & new config.

--- src/http-common.js
+++ src/http-common.js
@@ -1,12 +1,9 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://dummyapi.io/data/api'
-const APP_ID = '609d924de7ff743d0e88df1a'
-
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL:  process.env.REACT_APP_BASE_URL,
   headers: {
-    'app-id': APP_ID,
+    'app-id': process.env.REACT_APP_APP_ID,
     'Content-type': 'application/json'
   }
 })
